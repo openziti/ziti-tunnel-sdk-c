d341bfa40aa34c6f86c8c08521d689243ee984d9
Shawn Carey <shawn.carey@netfoundry.io>
2020-07-09 10:12:57 -0400

    added some lwip debug options

--- lib/lwip/lwipopts.h
+++ lib/lwip/lwipopts.h
@@ -5,23 +5,28 @@
 
 #define MEM_SIZE              524288      /* the size of the heap memory (1600) */
 
+#if SCAREY_DEBUGGING_LWIP
+#define MEMP_OVERFLOW_CHECK   2           /* reserves bytes before and after each memp element in every pool and fills it with a prominent default value */
+#define MEMP_SANITY_CHECK     1           /* run a sanity check after each mem_free() to make sure that the linked list of heap elements is not corrupted */
+//#define LWIP_ALLOW_MEM_FREE_FROM_OTHER_CONTEXT 0
+#endif
 //#define MEMP_NUM_PBUF       64          /* number of memp struct pbufs (used for PBUF_ROM and PBUF_REF) */
 
 #define MEMP_NUM_TCP_PCB      64          /* simultaneously active TCP connections (5) */
-#define MEMP_NUM_TCP_SEG      1024          /* simultaneously queued TCP segments (16) */
+#define MEMP_NUM_TCP_SEG      1024        /* simultaneously queued TCP segments (16) */
 
 #define PBUF_POOL_SIZE        512         /* number of buffers in the pbuf pool (16) */
 
-#define TCP_WND               0xffff     /* size of a TCP window. when using TCP_RCV_SCALE, TCP_WND is the total size with scaling applied (4 * TCP_MSS) */
-#define TCP_MSS               16384      /* TCP Maximum segment size (536) */
+#define TCP_WND               0xffff      /* size of a TCP window. when using TCP_RCV_SCALE, TCP_WND is the total size with scaling applied (4 * TCP_MSS) */
+#define TCP_MSS               16384       /* TCP Maximum segment size (536) */
 #define TCP_SND_BUF           TCP_WND     /* TCP sender buffer space in bytes (2 * TCP_MSS) */
 #define TCP_SND_QUEUELEN      64          /* TCP sender buffer space in pbufs ((4 * (TCP_SND_BUF) + (TCP_MSS - 1))/(TCP_MSS)) */
 // TCP_SNDQUEUELEN_OVERFLOW = 0xffffu - 3
 #define TCP_SNDLOWAT          (0xffff-(4*TCP_MSS)-1) /* TCP writable space (bytes). must be less than TCP_SND_BUF. the amount of space which must be available in the TCP snd_buf for select to return writable (combined with TCP_SNDQUEUELOWAT) LWIP_MIN(LWIP_MAX(((TCP_SND_BUF)/2), (2 * TCP_MSS) + 1), (TCP_SND_BUF) - 1) */
 #define LWIP_WND_SCALE        1           /* set to 1 to enable window scaling */
-#define TCP_RCV_SCALE         7          /* desired scaling factor - shift count in the range of [0..14] */
+#define TCP_RCV_SCALE         7           /* desired scaling factor - shift count in the range of [0..14] */
 
-#define LWIP_SINGLE_NETIF 1
+#define LWIP_SINGLE_NETIF 1               /* avoid some lwip "routing" logic */
 
 // APIs
 #define LWIP_RAW 1
@@ -29,8 +34,8 @@
 #define LWIP_SOCKET 0
 
 // protocols
-#define LWIP_IPV6 1                      /* enable ipv6 */
-#define IPV6_FRAG_COPYHEADER 1           /* avoid assert in lwip code when ipv6 is enabled */
+#define LWIP_IPV6 1                       /* enable ipv6 */
+#define IPV6_FRAG_COPYHEADER 1            /* avoid assert in lwip code when ipv6 is enabled */
 
 #ifdef _WIN32
 #define LWIP_NORAND 1
