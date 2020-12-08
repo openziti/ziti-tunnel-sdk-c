3a0992a152e47c660c3a54711d83c267ab88d1af
Shawn Carey <shawn.carey@netfoundry.io>
2020-12-08 10:48:32 -0500

    don't leak active connection list heads.

--- lib/tunnel_tcp.h
+++ lib/tunnel_tcp.h
@@ -19,6 +19,7 @@ extern int tunneler_tcp_close(struct tcp_pcb *pcb);
 
 extern int tunneler_tcp_close_write(struct tcp_pcb *pcb);
 
+/** return list of io contexts for active connections to the given service. caller must free the returned pointer */
 extern struct io_ctx_list_s *tunneler_tcp_active(const void *ztx, const char *service_name);
 
 #endif //ZITI_TUNNELER_SDK_TUNNELER_TCP_H
