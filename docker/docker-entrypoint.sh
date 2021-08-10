77f455f629f2ad9f1c53ce48ce47370bcfbaf277
Kenneth Bingham <w@qrk.us>
2021-08-10 13:23:14 -0400

    accept JWT as NF_REG_TOKEN or stdin (resolves #160)

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -23,34 +23,45 @@ if ! mountpoint "${persisted_dir}"; then
 fi
 
 # try to figure out the client name if it wasn't provided
-if [ -z "${NF_REG_NAME}" ]; then
-    if [ -n "${IOTEDGE_DEVICEID}" ]; then
+if [[ -z "${NF_REG_NAME}" ]]; then
+    if [[ -n "${IOTEDGE_DEVICEID}" ]]; then
         echo "INFO: setting NF_REG_NAME to \${IOTEDGE_DEVICEID} (${IOTEDGE_DEVICEID})"
         NF_REG_NAME="${IOTEDGE_DEVICEID}"
     fi
 fi
-if [ -z "${NF_REG_NAME}" ]; then
+if [[ -z "${NF_REG_NAME}" ]]; then
     echo "ERROR: please set the NF_REG_NAME environment variable when running this image" >&2
     exit 1
 fi
 
+# if not non-empty identity file then look for enrollment token
 json="${persisted_dir}/${NF_REG_NAME}.json"
-if [ ! -f "${json}" ]; then
+if [[ ! -s "${json}" ]]; then
     echo "INFO: identity configuration ${json} does not exist"
     for dir in "/var/run/secrets/netfoundry.io/enrollment-token" "${persisted_dir}"; do
         _jwt="${dir}/${NF_REG_NAME}.jwt"
         echo "INFO: looking for ${_jwt}"
-        if [ -f "${_jwt}" ]; then
+        if [[ -s "${_jwt}" ]]; then
             jwt="${_jwt}"
             break
         fi
     done
-    if [ -z "${jwt}" ]; then
-        echo "ERROR: ${NF_REG_NAME}.jwt was not found in the expected locations" >&2
-        exit 1
+    if [[ -n "${jwt:-}" ]]; then
+        echo "INFO: enrolling ${jwt}"
+        ziti-edge-tunnel enroll --jwt "${jwt}" --identity "${json}"
+    elif [[ -n "${NF_REG_TOKEN:-}" ]]; then
+        echo "INFO: attempting enrollment with NF_REG_TOKEN"
+        ziti-edge-tunnel enroll --jwt - --identity "${json}" <<< "${NF_REG_TOKEN}" || {
+            echo "ERROR: failed to enroll with token from NF_REG_TOKEN" >&2
+            exit 1
+        }
+    else
+        echo "INFO: ${NF_REG_NAME}.jwt was not found, trying stdin" >&2
+        ziti-edge-tunnel enroll --jwt - --identity "${json}" || {
+            echo "ERROR: failed to enroll with token from stdin" >&2
+            exit 1
+        }
     fi
-    echo "INFO: enrolling ${jwt}"
-    ziti-edge-tunnel enroll --jwt "${jwt}" --identity "${json}"
 fi
 
 echo "INFO: running ziti-edge-tunnel"
