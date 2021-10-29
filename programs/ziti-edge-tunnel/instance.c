070637a54727d8b31b7dcaef1609bd580d1dc74b
mary-dcouto <mary.dcouto@netfoundry.io>
2021-10-29 20:32:31 +0530

    move strdup into the function

--- programs/ziti-edge-tunnel/instance.c
+++ programs/ziti-edge-tunnel/instance.c
@@ -240,7 +240,7 @@ static tunnel_address *to_address(string hostOrIPOrCIDR) {
     } else {
         tnl_address->IsHost = true;
         tnl_address->IP = NULL;
-        tnl_address->HostName = hostOrIPOrCIDR;
+        tnl_address->HostName = strdup(hostOrIPOrCIDR);
         ZITI_LOG(TRACE, "Hostname: %s", hostOrIPOrCIDR);
     }
     // find CIDR
@@ -273,7 +273,7 @@ static void setTunnelServiceAddress(tunnel_service *tnl_svc, ziti_service *servi
         int address_idx;
         for(address_idx=0; cfg_v1.addresses[address_idx]; address_idx++) {
             char* addr = cfg_v1.addresses[address_idx];
-            tnl_addr_arr[address_idx] = to_address(strdup(addr));
+            tnl_addr_arr[address_idx] = to_address(addr);
         }
 
         // set protocols
@@ -296,7 +296,7 @@ static void setTunnelServiceAddress(tunnel_service *tnl_svc, ziti_service *servi
 
         // set tunnel address
         tnl_addr_arr = calloc(2, sizeof(tunnel_address *));
-        tnl_addr_arr[0] = to_address(strdup(zt_client_cfg_v1.hostname));
+        tnl_addr_arr[0] = to_address(zt_client_cfg_v1.hostname);
 
         // set protocols
         protocols = calloc(3, sizeof(char *));
