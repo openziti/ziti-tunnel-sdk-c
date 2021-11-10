67b332a72e5aea723d48b192f1a84d1403d17d37
mary-dcouto <mary.dcouto@netfoundry.io>
2021-11-10 18:10:25 +0530

    fixing the comment

--- programs/ziti-edge-tunnel/log_utils.c
+++ programs/ziti-edge-tunnel/log_utils.c
@@ -1,5 +1,5 @@
 /*
- Copyright $originalComment.match("Copyright (\d+)", 1, "-", "$today.year")2021 NetFoundry Inc.
+ Copyright 2019-2021 NetFoundry Inc.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
