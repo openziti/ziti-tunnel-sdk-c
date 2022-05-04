038de78c996e02763717ac53413fab6b3cca5f68
Shawn Carey <shawn.carey@netfoundry.io>
2022-05-04 09:33:49 -0400

    log_level can be const

--- programs/ziti-edge-tunnel/instance.c
+++ programs/ziti-edge-tunnel/instance.c
@@ -695,7 +695,7 @@ void set_ip_info(uint32_t dns_ip, uint32_t tun_ip, int bits) {
 
 }
 
-void set_log_level(char* log_level) {
+void set_log_level(const char* log_level) {
     if (log_level == NULL) {
         return;
     }
