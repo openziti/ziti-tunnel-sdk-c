7759f22405a4c8dc92a5fc6f32711c34a7a4e348
ekoby <7406535+ekoby@users.noreply.github.com>
2022-06-27 12:57:19 -0400

    log incoming/outgoing packets at netif driver level (#420)
    * log incoming/outgoing packets at netif driver level
    
    * minor formatting fix

--- lib/ziti-tunnel/ziti_tunnel_priv.h
+++ lib/ziti-tunnel/ziti_tunnel_priv.h
@@ -39,6 +39,53 @@ enum {
 if (tunnel_logger && level <= tunnel_log_level) { tunnel_logger(level, "tunnel-sdk", __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__); }\
 } while(0)
 
+
+static const char* proto_s[] = {
+        "HOPOPT",
+        "ICMP",
+        "IGMP",
+        "GGP",
+        "IP-in-IP",
+        "ST",
+        "TCP",
+        "CBT",
+        "EGP",
+        "IGP",
+        "BBN-RCC-MON",
+        "NVP-II",
+        "PUP",
+        "ARGUS",
+        "EMCON",
+        "XNET",
+        "CHAOS",
+        "UDP",
+};
+
+#define max_proto (sizeof(proto_s)/sizeof(proto_s[0]))
+
+#define _pkt(p) ((unsigned char*)(p))
+#define ip_ver(p) (_pkt(p)[0] >> 4)
+#define ip_proto(p) _pkt(p)[9]
+#define ip_protos(p) (ip_proto(p) < max_proto ? proto_s[ip_proto(p)] : "unk")
+
+#define ip_v4_src1(p) _pkt(p)[12]
+#define ip_v4_src2(p) _pkt(p)[13]
+#define ip_v4_src3(p) _pkt(p)[14]
+#define ip_v4_src4(p) _pkt(p)[15]
+#define ip_v4_src_port(p) ntohs(*(unsigned short*)((p) + (_pkt(p)[0] & 0xf) * 4))
+
+#define ip_v4_dst1(p) _pkt(p)[16]
+#define ip_v4_dst2(p) _pkt(p)[17]
+#define ip_v4_dst3(p) _pkt(p)[18]
+#define ip_v4_dst4(p) _pkt(p)[19]
+#define ip_v4_dst_port(p) ntohs(*(unsigned short*)((p) + (_pkt(p)[0] & 0xf) * 4 + 2))
+
+#define PACKET_FMT "%s[%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d]"
+
+#define PACKET_FMT_ARGS(p) ip_protos(p), \
+ip_v4_src1(p),ip_v4_src2(p),ip_v4_src3(p),ip_v4_src4(p),ip_v4_src_port(p), \
+ip_v4_dst1(p),ip_v4_dst2(p),ip_v4_dst3(p),ip_v4_dst4(p),ip_v4_dst_port(p)
+
 extern int tunnel_log_level;
 typedef void (*tunnel_logger_f)(int level, const char *module, const char *file, unsigned int line, const char *func, const char *fmt, ...);
 extern tunnel_logger_f tunnel_logger;
