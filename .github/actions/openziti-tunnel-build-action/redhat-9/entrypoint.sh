5f7b82009f52fdfb00c4ac420a14e8a569c088d4
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2022-10-13 12:14:17 -0400

    add redhat 9 build and package

---
+++ .github/actions/openziti-tunnel-build-action/redhat-9/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# RedHat 9
+#
+
+set -euo pipefail
+
+# these commands must be in the entrypoint so they are run after workspace is mounted on Docker workdir
+echo "INFO: GIT_DISCOVERY_ACROSS_FILESYSTEM=${GIT_DISCOVERY_ACROSS_FILESYSTEM}"
+echo "INFO: WORKDIR=${PWD}"
+echo "INFO: $(git --version)"
+
+# workspace dir for each build env is added to "safe" dirs in global config e.g.
+# ~/.gitconfig so both runner and builder containers trust these dirs
+# owned by different UIDs from that of Git's EUID. This is made necessary
+# by newly-enforced directory boundaries in Git v2.35.2
+# ref: https://lore.kernel.org/git/xmqqv8veb5i6.fsf@gitster.g/
+for SAFE in \
+    /github/workspace \
+    /__w/ziti-tunnel-sdk-c/ziti-tunnel-sdk-c \
+    /mnt ; do
+        git config --global --add safe.directory ${SAFE}
+done
+
+cmake -E make_directory ./build
+(
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_TOOLCHAIN_FILE=./toolchains/default.cmake \
+        -DBUILD_DIST_PACKAGES=ON \
+        -DUSE_OPENSSL=ON \
+        -S . \
+        -B ./build 
+    cmake \
+        --build ./build \
+        --target package \
+        --verbose
+)
+
+if (( ${#} )); then
+    echo "INFO: running ziti-edge-tunnel"
+    set -x
+    ./build/programs/ziti-edge-tunnel/ziti-edge-tunnel ${@}
+fi
