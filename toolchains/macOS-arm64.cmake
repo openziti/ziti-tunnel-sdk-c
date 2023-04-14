50cab58fcb4ac164fd10e10207a01e12eeff980a
Eugene K <eugene.kobyakov@netfoundry.io>
2023-04-14 15:41:24 -0400

    fix macOS arm64 toolchain

--- toolchains/macOS-arm64.cmake
+++ toolchains/macOS-arm64.cmake
@@ -5,8 +5,8 @@ set(CMAKE_SYSTEM_PROCESSOR arm64)
 
 set(ZITI_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 
-#SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -arch arm64")
-#SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch arm64")
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -arch arm64")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch arm64")
 #
 ## for libsodium
 #set(triple arm64-apple-macos11)
