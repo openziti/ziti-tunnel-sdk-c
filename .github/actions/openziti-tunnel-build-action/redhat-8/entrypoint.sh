fbd7d2225b10a88da41d7bc6b0093f492533d148
Shawn Carey <shawn.carey@netfoundry.io>
2023-04-21 21:03:58 -0400

    fix cmake --build argument order

--- .github/actions/openziti-tunnel-build-action/redhat-8/entrypoint.sh
+++ .github/actions/openziti-tunnel-build-action/redhat-8/entrypoint.sh
@@ -12,13 +12,13 @@ echo "INFO: $(git --version)"
 
 # if first positional is an expected arch string then set cmake preset,
 # else use ci-linux-x64 (which actually just uses native/host tools - e.g. not cross compile)
-if [ -n "${1}" ]; then
+if [ ${#} -ge 1 ]; then
     cmake_preset="${1}"
 else
     cmake_preset="ci-linux-x64"
 fi
 
-if [ -n "${2}" ]; then
+if [ ${#} -ge 2 ]; then
     cmake_config="${2}"
 else
     cmake_config="Release"
@@ -52,8 +52,8 @@ cmake -E make_directory ./build
             -B ./build 
     source scl_source enable gcc-toolset-10 \
         && cmake \
-            --config "${cmake_config}" \
             --build ./build \
+            --config "${cmake_config}" \
             --target package \
             --verbose
 )
