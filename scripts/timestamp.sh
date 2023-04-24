a2ab8d124ffa1f1b75df1c908022042f064b628b
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-04-24 13:07:27 -0400

    subtract the start offset

--- scripts/timestamp.sh
+++ scripts/timestamp.sh
@@ -11,11 +11,14 @@ set -euo pipefail
     exit 1
 }
 
-STARTSTAMP=$(grep -Pom1 'starting at \(\K[0-9-]+T[0-9:.]+' "${LOGFILE}")
+STARTSTAMP_LINE=$(grep -Pm1 'starting at \([0-9-]+T[0-9:.]+\)' "${LOGFILE}")
+STARTOFFSET=$(grep -Po '^\([0-9]+\)\[(\s+)?\K[0-9.]+' <<< "${STARTSTAMP_LINE}")
+STARTSTAMP=$(grep -Po 'starting at \(\K[0-9-]+T[0-9:.]+' <<< "${STARTSTAMP_LINE}")
+REALSTARTSTAMP=$(date --utc --date "${STARTSTAMP} -${STARTOFFSET} seconds" +%Y-%m-%dT%H:%M:%S.%3NZ)
 
 while read -r; do
     OFFSET=$(grep -Po '^\([0-9]+\)\[(\s+)?\K[0-9.]+' <<< "${REPLY}")
     MESSAGE=$(grep -Po '^\([0-9]+\)\[(\s+)?[0-9.]+(\s+)?\]\K.*' <<< "${REPLY}")
-    UTCSTAMP=$(date --utc --date "${STARTSTAMP} +${OFFSET} seconds" +%Y-%m-%dT%H:%M:%S.%3NZ)
+    UTCSTAMP=$(date --utc --date "${REALSTARTSTAMP} +${OFFSET} seconds" +%Y-%m-%dT%H:%M:%S.%3NZ)
     echo "${UTCSTAMP} ${MESSAGE}"
 done < "${LOGFILE}"
