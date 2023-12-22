81d5574d46d18e26bdf713699807f6870cd4f41a
Shawn Carey <shawn.carey@netfoundry.io>
2023-12-22 10:38:11 -0500

    fixed comment

--- toolchains/Windows-x86.cmake
+++ toolchains/Windows-x86.cmake
@@ -1,4 +1,4 @@
-# cross-compile for windows/arm64 on windows/x64 host with visual studio
+# cross-compile for windows/x86 on windows host with visual studio
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR X86)
 #set(CMAKE_GENERATOR_PLATFORM X86)
