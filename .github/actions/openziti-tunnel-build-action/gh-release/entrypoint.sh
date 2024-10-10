40ae1cd4ff2029292c2c2f0d355667f612bc4f2b
Shawn Carey <shawn.carey@netfoundry.io>
2024-10-10 09:31:19 -0400

    use openziti-tunnel-build-action to run cmake builds for linux

---
+++ .github/actions/openziti-tunnel-build-action/gh-release/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024 NetFoundry Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# RedHat 8
+#
+
+set -euo pipefail
+
+# these commands must be in the entrypoint so they are run after workspace is mounted on Docker workdir
+echo "INFO: GIT_DISCOVERY_ACROSS_FILESYSTEM=${GIT_DISCOVERY_ACROSS_FILESYSTEM}"
+echo "INFO: WORKDIR=${PWD}"
+echo "INFO: $(git --version)"
+
+# if first positional is an expected arch string then set cmake preset,
+# else use ci-linux-x64 (which actually just uses native/host tools - e.g. not cross compile)
+if [ ${#} -ge 1 ]; then
+    cmake_preset="${1}"
+else
+    cmake_preset="ci-linux-x64"
+fi
+
+if [ ${#} -ge 2 ]; then
+    cmake_config="${2}"
+else
+    cmake_config="RelWithDebInfo"
+fi
+
+# workspace dir for each build env is added to "safe" dirs in global config e.g.
+# ~/.gitconfig so both runner and builder containers trust these dirs
+# owned by different UIDs from that of Git's EUID. This is made necessary
+# by newly-enforced directory boundaries in Git v2.35.2
+# ref: https://lore.kernel.org/git/xmqqv8veb5i6.fsf@gitster.g/
+for SAFE in \
+    /github/workspace \
+    /__w/ziti-tunnel-sdk-c/ziti-tunnel-sdk-c \
+    /mnt ; do
+        git config --global --add safe.directory ${SAFE}
+done
+
+cmake -E make_directory ./build
+(
+    [[ -d ./build ]] && rm -r ./build
+    cmake -E make_directory ./build
+    # allow unset for scl_source scripts
+    set +u
+    source scl_source enable gcc-toolset-10 \
+        && cmake \
+            --preset "${cmake_preset}" \
+            -DCMAKE_BUILD_TYPE="${cmake_config}" \
+            -DBUILD_DIST_PACKAGES=ON \
+            -S . \
+            -B ./build
+    source scl_source enable gcc-toolset-10 \
+        && cmake \
+            --build ./build \
+            --config "${cmake_config}" \
+            --target package \
+            --verbose
+)
