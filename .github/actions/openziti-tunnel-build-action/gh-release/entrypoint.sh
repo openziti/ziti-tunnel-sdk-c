0cb7c78ff2821f6092d2b4ef5209601a7d6d73ad
Shawn Carey <shawn.carey@netfoundry.io>
2024-10-10 21:19:09 -0400

    use ziti-builder:v1 to generate GH release builds (#1015)
    link release binaries with glibc 2.2.7

---
+++ .github/actions/openziti-tunnel-build-action/gh-release/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024 NetFoundry Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -euo pipefail
+
+# these commands must be in the entrypoint so they are run after workspace is mounted on Docker workdir
+echo "INFO: GIT_DISCOVERY_ACROSS_FILESYSTEM=${GIT_DISCOVERY_ACROSS_FILESYSTEM}"
+echo "INFO: WORKDIR=${PWD}"
+echo "INFO: $(git --version)"
+
+# if first positional is an expected arch string then set cmake preset,
+# else use ci-linux-x64 (which actually just uses native/host tools - e.g. not cross compile)
+if [ ${#} -ge 1 ]; then
+    cmake_preset="${1}"
+else
+    cmake_preset="ci-linux-x64"
+fi
+
+if [ ${#} -ge 2 ]; then
+    cmake_config="${2}"
+else
+    cmake_config="RelWithDebInfo"
+fi
+
+# workspace dir for each build env is added to "safe" dirs in global config e.g.
+# ~/.gitconfig so both runner and builder containers trust these dirs
+# owned by different UIDs from that of Git's EUID. This is made necessary
+# by newly-enforced directory boundaries in Git v2.35.2
+# ref: https://lore.kernel.org/git/xmqqv8veb5i6.fsf@gitster.g/
+for SAFE in \
+    /github/workspace \
+    /__w/ziti-tunnel-sdk-c/ziti-tunnel-sdk-c \
+    /mnt ; do
+        git config --global --add safe.directory ${SAFE}
+done
+
+export VCPKG_ROOT="${PWD}/vcpkg"
+# ${VCPKG_ROOT}/packages  will have been populated from outside of the container, by a different user. tell git it's ok.
+git config --global --add safe.directory "${VCPKG_ROOT}"
+
+if [ ! -d "${VCPKG_ROOT}/ports" ]; then
+    # the packages/ directory may have been populated from cache by now
+    # but git clone refuses to clone into a non-empty directory, so get
+    # vcpkg without using clone
+    git init "${VCPKG_ROOT}"
+    (cd "${VCPKG_ROOT}"; git remote add -f origin https://github.com/microsoft/vcpkg; git checkout master)
+    "${VCPKG_ROOT}/bootstrap-vcpkg.sh" -disableMetrics
+fi
+
+cmake -E make_directory ./build
+cmake \
+  --preset "${cmake_preset}" \
+  -DCMAKE_BUILD_TYPE="${cmake_config}" \
+  -S . \
+  -B ./build
+cmake \
+  --build ./build \
+  --config "${cmake_config}" \
+  --target bundle \
+  --verbose
