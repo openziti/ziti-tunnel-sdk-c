19b97ea255d76e0c0c499886fdeefd8db68404b3
Tom Carroll <34632752+tomc797@users.noreply.github.com>
2024-10-13 21:07:08 -0700

    Add support for Linux capabilities (7).
    Add support to adjust and restore thread capabilities. This is necessary as the `SO_MARK` socket option requires the `CAP_NET_ADMIN` capability for correct operation; otherwise, permission is denied.
    
    Signed-off-by: Tom Carroll <4632752+tomc797@users.noreply.github.com>

---
+++ programs/ziti-edge-tunnel/netif_driver/linux/capability.c
@@ -0,0 +1,163 @@
+#include "capability.h"
+
+#define _GNU_SOURCE
+
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <linux/capability.h>
+#include <sysexits.h>
+
+#include <ziti/ziti_log.h>
+
+#ifndef _LINUX_CAPABILITY_VERSION_1
+#  define _LINUX_CAPABILITY_VERSION_1  0x19980330
+#endif
+
+#ifndef _LINUX_CAPABILITY_U32S_1
+#  define _LINUX_CAPABILITY_U32S_1      1
+#endif
+
+#ifndef _LINUX_CAPABILITY_VERSION_3
+#  define _LINUX_CAPABILITY_VERSION_3   0x20080522
+#endif
+
+#ifndef _LINUX_CAPABILITY_U32S_3
+#  define _LINUX_CAPABILITY_U32S_3      2
+#endif
+
+/* declarations not provided */
+extern int capget(cap_user_header_t cap_header, cap_user_data_t cap_data);
+extern int capset(cap_user_header_t cap_header, const cap_user_data_t cap_data);
+
+#ifndef ZITI_TUNNELER_SDK_HAVE_CAPGET
+/**
+ * not all libc's provide wrappers
+ */
+
+#include <sys/syscall.h>
+
+int
+capget(cap_user_header_t header, cap_user_data_t data)
+{
+    return (int) syscall(SYS_capget, header, data);
+}
+
+int
+capset(cap_user_header_t header, const cap_user_data_t data)
+{
+    return (int) syscall(SYS_capset, header, data);
+}
+
+#endif /* ZITI_TUNNELER_SDK_HAVE_CAPGET */
+
+struct cap_struct {
+    struct __user_cap_header_struct header;
+    struct __user_cap_data_struct data[_LINUX_CAPABILITY_U32S_3 > _LINUX_CAPABILITY_U32S_1 ? _LINUX_CAPABILITY_U32S_3 : _LINUX_CAPABILITY_U32S_1];
+};
+
+static __thread struct {
+    struct cap_struct saved_cap;
+    bool armed;
+} thread_state = {
+  .saved_cap.header = { _LINUX_CAPABILITY_VERSION_3 },
+};
+
+static int
+ziti__cap_assert(unsigned long linux_cap_mask, unsigned long flags)
+{
+    struct cap_struct cap = { .header = thread_state.saved_cap.header };
+    struct cap_struct oldcap;
+    int sys_rc;
+
+    (void) flags;
+
+    sys_rc = capget(&cap.header, cap.data);
+    if (sys_rc && errno == EINVAL && cap.header.version < thread_state.saved_cap.header.version) {
+        cap.header.version = _LINUX_CAPABILITY_VERSION_1;
+        sys_rc = capget(&cap.header, cap.data);
+    }
+
+    if (sys_rc < 0) {
+        int saved_errno = errno;
+
+        ZITI_LOG(ERROR, "failed to get thread's capabilities: (%d) %s",
+            -saved_errno, strerror(saved_errno));
+        return -saved_errno;
+    }
+
+    oldcap = cap;
+
+    cap.data[0].effective |= linux_cap_mask;
+    cap.data[0].permitted |= linux_cap_mask;
+
+    sys_rc = capset(&cap.header, cap.data);
+    if (sys_rc < 0) {
+        int saved_errno = errno;
+
+        ZITI_LOG(ERROR, "failed to set thread capabilities: (%d) %s",
+            -saved_errno, strerror(saved_errno));
+        return -saved_errno;
+    }
+
+    if (!thread_state.armed)
+        thread_state.saved_cap = oldcap;
+
+    thread_state.armed = true;
+
+    return 0;
+}
+
+static int
+ziti__cap_restore(void)
+{
+    int sys_rc;
+
+    if (!thread_state.armed) {
+        ZITI_LOG(ERROR, "no previously saved capabilitity to restore");
+        return -EINVAL;
+    }
+
+    sys_rc = capget(&thread_state.saved_cap.header, thread_state.saved_cap.data);
+    if (sys_rc < 0) {
+        int saved_errno = errno;
+
+        ZITI_LOG(ERROR, "failed to restore thread's capabilities: (%d) %s",
+            -saved_errno, strerror(saved_errno));
+        return -saved_errno;
+    }
+
+    thread_state.armed = false;
+
+    return 0;
+}
+
+#define ZITI__LINUX_CAP_VALID(capability)                              \
+  static_assert(CAP_TO_INDEX(capability)==0, "capability ordinal is too large for the available bit set.")
+
+ZITI__LINUX_CAP_VALID(CAP_NET_ADMIN);
+ZITI__LINUX_CAP_VALID(CAP_SYS_ADMIN);
+
+void
+ziti_cap_assert(unsigned long cap_mask)
+{
+    unsigned long linux_cap_mask =
+      ((cap_mask & ZITI_CAP_NETADMIN) ? CAP_TO_MASK(CAP_NET_ADMIN) : 0)
+      | ((cap_mask & ZITI_CAP_SYSADMIN) ? CAP_TO_MASK(CAP_SYS_ADMIN) : 0);
+
+    if (ziti__cap_assert(linux_cap_mask, 0) < 0)
+        exit(EX_NOPERM);
+}
+
+void
+ziti_cap_restore(void)
+{
+    int sys_rc;
+
+    sys_rc = ziti__cap_restore();
+    if (sys_rc < 0)
+        exit(EX_NOPERM);
+}
