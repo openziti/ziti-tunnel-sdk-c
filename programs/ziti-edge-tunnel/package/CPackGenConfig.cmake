3056a8c806276854c9584d0da2e352463d92184d
Shawn Carey <shawn.carey@netfoundry.io>
2024-10-14 12:05:23 -0400

    link with distro's libprotobuf-c

--- programs/ziti-edge-tunnel/package/CPackGenConfig.cmake
+++ programs/ziti-edge-tunnel/package/CPackGenConfig.cmake
@@ -1,7 +1,7 @@
 cmake_policy(SET CMP0057 NEW)
 
 if(CPACK_GENERATOR MATCHES "RPM")
-    set(CPACK_RPM_BUILDREQUIRES "cmake >= ${CMAKE_MINIMUM_REQUIRED_VERSION}, systemd-devel, gawk, gcc-c++ >= 4.9, python3, openssl-devel, zlib-devel")
+    set(CPACK_RPM_BUILDREQUIRES "cmake >= ${CMAKE_MINIMUM_REQUIRED_VERSION}, systemd-devel, gawk, gcc-c++ >= 4.9, protobuf-c, python3, openssl-devel, zlib-devel")
     if(CPACK_OS_RELEASE_NAME IN_LIST CPACK_RPM_DISTRIBUTIONS AND CPACK_OS_RELEASE_VERSION VERSION_GREATER "7")
         list(APPEND CPACK_RPM_BUILDREQUIRES "systemd-rpm-macros")
     endif()
@@ -39,6 +39,6 @@ if(CPACK_GENERATOR MATCHES "DEB")
     # specify "libssl3 if it exists in the repos, or nothing" as a dependency.
     # systemd package on older distros does not contain `systemd-sysusers`, so include passwd for `useradd`, `groupadd`.
     # login provides `/usr/sbin/nologin`.
-    set(CPACK_DEBIAN_PACKAGE_DEPENDS "debconf, iproute2, sed, systemd, libatomic1, libssl3 | libssl1.1 | libssl1.0.0, login, passwd, policykit-1, zlib1g")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "debconf, iproute2, sed, systemd, libatomic1, libprotobuf-c1, libssl3 | libssl1.1 | libssl1.0.0, login, passwd, policykit-1, zlib1g")
     set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CPACK_DEB_CONFFILES};${CPACK_DEB_PRE_INSTALL};${CPACK_DEB_POST_INSTALL};${CPACK_DEB_PRE_UNINSTALL};${CPACK_DEB_POST_UNINSTALL};${CPACK_DEB_TEMPLATES}")
 endif(CPACK_GENERATOR MATCHES "DEB")
