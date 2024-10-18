a52fb864e761506df933a902756650812c88d67f
Tom Carroll <34632752+tomc797@users.noreply.github.com>
2024-10-18 16:57:36 -0700

    Add socket factory that configures `SO_MARK` on newly created sockets.
    Implemented a socket factory that configures the socket option `SO_MARK` to bypass the ZITI routing table, ensuring proper packet marking for custom routing.
    
    Signed-off-by: Tom Carroll <4632752+tomc797@users.noreply.github.com>

--- programs/ziti-edge-tunnel/netif_driver/linux/tun.c
+++ programs/ziti-edge-tunnel/netif_driver/linux/tun.c
@@ -34,6 +34,7 @@
 #include "tun.h"
 #include "utils.h"
 #include "libiproute.h"
+#include "capability.h"
 
 #ifndef DEVTUN
 #define DEVTUN "/dev/net/tun"
@@ -587,4 +588,64 @@ netif_driver tun_open(uv_loop_t *loop, uint32_t tun_ip, uint32_t dns_ip, const c
     }
 
     return driver;
+}
+
+static int make_bypass_socket(const struct addrinfo *ai, bool blocking)
+{
+    int blockmode = blocking ? 0 : SOCK_NONBLOCK;
+    int sd;
+
+    sd = socket(ai->ai_family, ai->ai_socktype|SOCK_CLOEXEC|blockmode,
+        ai->ai_protocol);
+    if (sd < 0) {
+        int uv_err = uv_translate_sys_error(errno);
+
+        ZITI_LOG(ERROR, "Failed to create socket: %d/%s", uv_err, uv_strerror(uv_err));
+        return uv_err;
+    }
+
+    int mark = ZET_BYPASS_MARK;
+    int sys_rc;
+
+    ziti_cap_assert(ZITI_CAP_NETADMIN);
+    sys_rc = setsockopt(sd, SOL_SOCKET, SO_MARK, &mark, sizeof mark);
+    ziti_cap_restore();
+
+    if (sys_rc < 0) {
+        int uv_err = uv_translate_sys_error(errno);
+
+        ZITI_LOG(ERROR, "Failed to configure SO_MARK on socket: %d/%s",
+            uv_err, uv_strerror(uv_err));
+        (void) close(sd);
+        return uv_err;
+    }
+
+    return sd;
+}
+
+/**
+ * Override tlsuv's socket factory. This factory is used to create
+ * the sockets used for the controller and edge router communication.
+ */
+uv_os_sock_t tlsuv_socket(const struct addrinfo *ai, bool blocking)
+{
+    int blockopt = blocking ? 0 : SOCK_NONBLOCK;
+    uv_os_sock_t /* int */ sd;
+
+    sd = socket(ai->ai_family, ai->ai_socktype|SOCK_CLOEXEC|blockopt, ai->ai_protocol);
+    if (sd < 0) {
+        int uv_err = uv_translate_sys_error(errno);
+
+        ZITI_LOG(ERROR, "socket: %d/%s", uv_err, uv_strerror(uv_err));
+        return -1;
+    }
+
+    int mark = ZET_BYPASS_MARK;
+    if (setsockopt(sd, SOL_SOCKET, SO_MARK, &mark, sizeof mark) < 0) {
+        int uv_err = uv_translate_sys_error(errno);
+
+        ZITI_LOG(WARN, "setsockopt(SO_MARK): %d/%s", uv_err, uv_strerror(uv_err));
+    }
+
+    return sd;
 }
