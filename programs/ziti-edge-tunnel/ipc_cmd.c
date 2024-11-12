83904919e5f961f754226793cac70950d0eb8876
Shawn Carey <shawn.carey@netfoundry.io>
2024-11-12 06:46:30 -0500

    get include from installed directory

--- programs/ziti-edge-tunnel/ipc_cmd.c
+++ programs/ziti-edge-tunnel/ipc_cmd.c
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <json_tokener.h>
+#include <json-c/json_tokener.h>
 #include <stdlib.h>
 #include <uv.h>
 #include <tlsuv/queue.h>
