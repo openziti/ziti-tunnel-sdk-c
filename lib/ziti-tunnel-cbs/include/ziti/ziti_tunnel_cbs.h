f713ea1f80f489acc0108b63228c71b2dd86ed97
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-11-20 21:54:37 -0500

    updates to lib

--- lib/ziti-tunnel-cbs/include/ziti/ziti_tunnel_cbs.h
+++ lib/ziti-tunnel-cbs/include/ziti/ziti_tunnel_cbs.h
@@ -184,7 +184,8 @@ XX(jwtFileName, model_string, none, JwtFileName, __VA_ARGS__) \
 XX(jwtContent, model_string, none, JwtContent, __VA_ARGS__) \
 XX(key, model_string, none, Key, __VA_ARGS__) \
 XX(cert, model_string, none, Certificate, __VA_ARGS__) \
-XX(controllerURL, model_string, none, ControllerURL, __VA_ARGS__)
+XX(controllerURL, model_string, none, ControllerURL, __VA_ARGS__) \
+XX(alias, model_string, none, Alias, __VA_ARGS__)
 
 #define TUNNEL_EXT_AUTH(XX, ...) \
 XX(identifier, model_string, none, identifier, __VA_ARGS__) \
