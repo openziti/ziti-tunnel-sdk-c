25b164d8c2c99f5bbb7429211cd1052ded92a1cb
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-12-17 10:34:17 -0500

    move until after all the finds are done

--- programs/ziti-edge-tunnel/netif_driver/windows/tun.c
+++ programs/ziti-edge-tunnel/netif_driver/windows/tun.c
@@ -212,7 +212,6 @@ netif_driver tun_open(struct uv_loop_s *loop, uint32_t tun_ip, const char *cidr,
     swprintf(w_adapter_name, MAX_ADAPTER_NAME, L"%hs", tun->name);
 
     WINTUN_ADAPTER_HANDLE found = WintunOpenAdapter(w_adapter_name);
-    WintunSetLogger(WintunLogger); //set the logger here exclusively to avoid the error: Failed to find matching adapter name: Element not found. (Code 0x00000490)
     while (found) {
         tun_num++;
         WintunCloseAdapter(found); // already exists. increment and try again
@@ -226,6 +225,7 @@ netif_driver tun_open(struct uv_loop_s *loop, uint32_t tun_ip, const char *cidr,
         swprintf(w_adapter_name, MAX_ADAPTER_NAME, L"%hs", tun->name);
         found = WintunOpenAdapter(w_adapter_name);
     }
+    WintunSetLogger(WintunLogger); //set the logger here exclusively to avoid these errors from WinTun: Failed to find matching adapter name: Element not found. (Code 0x00000490)
 
     tun->adapter = WintunCreateAdapter(w_adapter_name, L"OpenZiti", NULL); // Wintun adds "Tunnel" so this will be "OpenZiti Tunnel"
     if (!tun->adapter) {
