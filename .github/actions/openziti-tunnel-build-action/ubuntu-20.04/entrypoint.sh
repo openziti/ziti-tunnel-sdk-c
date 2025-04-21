3693bbe4034844710bd79f3a3bdd02a20f4ad090
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2025-04-21 15:23:21 -0400

    install build essentials after multi-arch on focal

--- .github/actions/openziti-tunnel-build-action/ubuntu-20.04/entrypoint.sh
+++ .github/actions/openziti-tunnel-build-action/ubuntu-20.04/entrypoint.sh
@@ -3,7 +3,10 @@
 # Debian Bullseye/Ubuntu Focal 20.04
 #
 
-set -euo pipefail
+set -euxo pipefail
+
+# Add Vcpkg verbose flag
+export VCPKG_VERBOSE=1
 
 # these commands must be in the entrypoint so they are run after workspace is mounted on Docker workdir
 echo "INFO: GIT_DISCOVERY_ACROSS_FILESYSTEM=${GIT_DISCOVERY_ACROSS_FILESYSTEM}"
@@ -44,6 +47,7 @@ done
 )
 
 [[ -d ./build ]] && rm -r ./build
+
 cmake \
     -E make_directory \
     ./build/
@@ -52,8 +56,15 @@ cmake \
     -DCMAKE_BUILD_TYPE="${cmake_config}" \
     -DVCPKG_OVERLAY_PORTS=./.github/actions/openziti-tunnel-build-action/ubuntu-20.04/vcpkg-overlays \
     -DBUILD_DIST_PACKAGES=ON \
+    "${TLSUV_TLSLIB:+-DTLSUV_TLSLIB=${TLSUV_TLSLIB}}" \
     -S "${PWD}/" \
-    -B ./build/
+    -B ./build \
+    || {
+        echo "CMake configuration failed. Dumping vcpkg detect_compiler error log:"
+        cat /usr/local/vcpkg/buildtrees/detect_compiler/config-x64-linux-rel-err.log
+        exit 1
+    }
+
 cmake \
     --build ./build/ \
     --config "${cmake_config}" \
