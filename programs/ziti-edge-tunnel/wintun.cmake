e58f24157a2e5eca2c24b4f54b0dd8b61615ddd7
Shawn Carey <shawn.carey@netfoundry.io>
2025-10-28 12:40:29 -0400

    get wintun with FetchContent_MakeAvailable (#1220)

--- programs/ziti-edge-tunnel/wintun.cmake
+++ programs/ziti-edge-tunnel/wintun.cmake
@@ -5,10 +5,7 @@ FetchContent_Declare(wintun
         DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 
-FetchContent_GetProperties(wintun)
-if(NOT wintun_POPULATED)
-    FetchContent_Populate(wintun)
-endif()
+FetchContent_MakeAvailable(wintun)
 
 add_library(wintun INTERFACE)
 target_include_directories(wintun INTERFACE ${wintun_SOURCE_DIR}/include)
