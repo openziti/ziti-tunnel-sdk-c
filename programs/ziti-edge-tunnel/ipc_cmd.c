83507c9020f8f2dfa4e292b789cff68b1bfed0e8
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2026-03-07 14:43:43 -0500

    scope NRPT rule operations to owning instance; remove orphans on startup

--- programs/ziti-edge-tunnel/ipc_cmd.c
+++ programs/ziti-edge-tunnel/ipc_cmd.c
@@ -170,7 +170,9 @@ static void on_command_resp(const tunnel_result* result, void *ctx) {
                                 }
 
                                 if (model_map_size(&hostnamesToRemove) > 0) {
-                                    remove_nrpt_rules(global_loop_ref, &hostnamesToRemove, ipc_discriminator);
+                                    char *zet_id = get_zet_instance_id(ipc_discriminator);
+                                    remove_nrpt_rules(global_loop_ref, &hostnamesToRemove, zet_id);
+                                    free(zet_id);
                                 }
                             }
                         } else {
