db16c769836a506b3a02cef19cd60a7f5aade4a9
Shawn Carey <shawn.carey@netfoundry.io>
2026-05-15 14:40:53 -0400

    set vcpkg_make_host_triplet in linux triplet files

--- vcpkg-triplets/mipsel-linux.cmake
+++ vcpkg-triplets/mipsel-linux.cmake
@@ -4,3 +4,4 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 # ci-linux-mipsel currently chainloads Linux-arm.cmake; preserved here until a correct mipsel toolchain is added
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/Linux-arm.cmake")
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=mipsel-linux-gnu")
