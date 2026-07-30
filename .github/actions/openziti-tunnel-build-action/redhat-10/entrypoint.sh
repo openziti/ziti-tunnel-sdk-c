d7bca01e185040cf8de0d698c3d2f5b9cd0b7422
Shawn Carey <shawn.carey@netfoundry.io>
2026-07-30 16:32:33 -0400

    set VCPKG_OVERLAY_PORTS correctly for redhat-10

--- .github/actions/openziti-tunnel-build-action/redhat-10/entrypoint.sh
+++ .github/actions/openziti-tunnel-build-action/redhat-10/entrypoint.sh
@@ -59,7 +59,7 @@ done
     cmake \
         --preset "${cmake_preset}" \
         -DCMAKE_BUILD_TYPE="${cmake_config}" \
-        -DVCPKG_OVERLAY_PORTS=./.github/actions/openziti-tunnel-build-action/redhat-9/vcpkg-overlays \
+        -DVCPKG_OVERLAY_PORTS=./.github/actions/openziti-tunnel-build-action/redhat-10/vcpkg-overlays \
         -DBUILD_DIST_PACKAGES=ON \
         "${TLSUV_TLSLIB:+-DTLSUV_TLSLIB=${TLSUV_TLSLIB}}" \
         -S . \
